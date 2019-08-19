549f9dc475be7344461ba31a0d99b005495a4f35
kkboom <1396956549@qq.com>
2019-08-19 22:09:17 +0800

    switch base_yrl

--- front/src/main.js
+++ front/src/main.js
@@ -1,29 +1,29 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import iview from 'iview'
-import App from './App'
-import router from './router'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import mavonEditor from 'mavon-editor'
-import 'mavon-editor/dist/css/index.css'
+import Vue from "vue";
+import iview from "iview";
+import App from "./App";
+import router from "./router";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import mavonEditor from "mavon-editor";
+import "mavon-editor/dist/css/index.css";
 
-
-Vue.use(iview)
-Vue.use(mavonEditor)
-Vue.config.productionTip = false
+Vue.use(iview);
+Vue.use(mavonEditor);
+Vue.config.productionTip = false;
 
 // axios 配置
 axios.defaults.timeout = 8000;
 // axios.defaults.baseURL = 'http://www.kkboom.cn:9001';
-axios.defaults.baseURL = 'http://localhost:9001';
+axios.defaults.baseURL = "http://localhost:9001";
 
 // http request 拦截器
 axios.interceptors.request.use(
   config => {
-    if (localStorage.token) { //判断token是否存在
-      config.headers.Authorization = localStorage.token;  //将token设置成请求头
+    if (localStorage.token) {
+      //判断token是否存在
+      config.headers.Authorization = localStorage.token; //将token设置成请求头
     }
     return config;
   },
@@ -31,13 +31,12 @@ axios.interceptors.request.use(
     return Promise.reject(err);
   }
 );
-Vue.use(VueAxios, axios)
-
+Vue.use(VueAxios, axios);
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
